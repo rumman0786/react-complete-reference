640d43e895112e6c21c0b38df319a9c7f699d25f
Rumman Ashraf <rumman.ashraf@gmail.com>
2020-12-26 12:36:42 +0600

    Added props sample with custom components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>This is a React App</h1>
+        <p>Does this work???</p>
+        <Person name="Rumman" age="30"/>
+        <Person name="Sabrina" age="25">My Hobbies: Browsing Netflix, Learning Coding</Person>
+        <Person name="Max" age="27"/>
       </div>
     );
+    // return React.createElement('div', {className: 'App'},
+    //                                 React.createElement('h1', null, 'Hellow World!!!'));
   }
 }
 
