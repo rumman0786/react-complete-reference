a6d9fb01f6e9d9232bff4c3a36e7087a117f4b99
Rumman Ashraf <rumman.ashraf@gmail.com>
2020-12-26 23:27:41 +0600

    Added state management with setState

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Rumman', age:30},
+      {name: 'Sabrina', age:25},
+      {name: 'Max', age:27},
+    ],
+    otherstate: "This is another prop"
+  }
+
+  switchNameHandler = () => {
+    // console.log('Clicked switch button');
+    this.setState({
+      persons: [
+        {name: 'Rumman Bin Ashraf', age:30},
+        {name: 'Sabrina', age:25},
+        {name: 'Max', age:35},
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>This is a React App</h1>
         <p>Does this work???</p>
-        <Person name="Rumman" age="30"/>
-        <Person name="Sabrina" age="25">My Hobbies: Browsing Netflix, Learning Coding</Person>
-        <Person name="Max" age="27"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Browsing Netflix, Learning Coding</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'},
