775e2062ce58511fec712b5e82ffe5fdef8e0197
Rumman Ashraf <rumman.ashraf@gmail.com>
2020-12-27 00:43:02 +0600

    Added jsx Inline styling

--- src/App.js
+++ src/App.js
@@ -35,11 +35,20 @@ class App extends Component {
   }
 
   render() {
+    const jsxButtonstyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>This is a React App</h1>
         <p>Does this work???</p>
-        <button onClick={() => this.switchNameHandler('Rumman!!!')}>Switch Name</button>
+        <button style={jsxButtonstyle}
+                onClick={() => this.switchNameHandler('Rumman!!!')}>Switch Name</button>
         <Person name={this.state.persons[0].name}
                 age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Person.css'
 
 const person = (props) => {
+
     return (
         <div className='Person'>
             <p onClick={props.callback}>I am {props.name}, I am {props.age} years old!!!</p>
