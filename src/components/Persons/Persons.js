5fde90f68d8eb9ecb7c1e9a28e10d321c7d8c94f
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-01-09 20:24:47 +0600

    Added LifeCycle Hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Person from './Person/Person'
 
-const Persons = (props) => props.persons.map(
-    (person, index) => {
-        return <Person
-            callback={() => props.clicked(index)}
-            changeCallback={(event) => props.changed(event, person.id)}
-            key={person.id}
-            name={person.name}
-            age={person.age}/>
-    }
-);
+const Persons = (props) => {
+    console.log('[Persons.js] render');
+
+    return props.persons.map(
+        (person, index) => {
+            return <Person
+                callback={() => props.clicked(index)}
+                changeCallback={(event) => props.changed(event, person.id)}
+                key={person.id}
+                name={person.name}
+                age={person.age}/>
+        }
+    )
+};
 
 export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {id: 1, name: 'Rumman', age:30},
@@ -13,6 +18,19 @@ class App extends Component {
     ],
     otherstate: "This is another prop",
     showPersons: false,
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   switchNameHandler = (newName) => {
@@ -57,6 +75,8 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
+
     let persons = null;
     
     if(this.state.showPersons) {
