11cbdc472f56dc4a32f272cfcdf00f9bfc5c7aaa
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-01-15 21:26:36 +0600

    Added functional higher oder component wrapper

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Aux from '../../../aux/aux';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 
@@ -98,7 +99,7 @@ class App extends Component {
     }
 
      return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button 
           onClick={() => this.setState({showCockpit: false})}
         >Remove Cockpit</button>
@@ -112,7 +113,7 @@ class App extends Component {
         
         : null}
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'},
     //                                 React.createElement('h1', null, 'Hellow World!!!'));
